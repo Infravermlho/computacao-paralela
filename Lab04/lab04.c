16f42a4e96e59889a5d0d0c3d940af81da34fabc
Vinícius Faria <64180986+ViniciusFaria-png@users.noreply.github.com>
2022-04-21 00:37:04 -0300

    Update lab04.c

--- Lab04/lab04.c
+++ Lab04/lab04.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/wait.h>   
 
+int main()
+{
+    pid_t pid;
+    int ShmID; 
+    int size = 1024;
+    int *value;
+    
+    ShmID = shmget(IPC_PRIVATE, size, IPC_CREAT | 0666);
+    if(ShmID < 0){
+      printf("Erro no shmget\n");
+      exit(1);
+    }
+    
+    value = (int*) shmat(ShmID, NULL, 0);
+    *value = 1;
+    printf("Valor inicial: %i\n", *value);
+    pid = fork();
+    if (pid == 0)
+    {     
+        *value += 2; 
+        printf("Valor apos a soma: %i\n",*value);    
+    }
+    else
+    {
+        wait(-1);     
+        *value *= 4;
+        printf("Valor apos a multiplicacao: %i\n",*value);    
+    }
+    
+    exit(0);
+}
