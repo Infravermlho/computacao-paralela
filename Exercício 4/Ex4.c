98755e9a5acab02b902c8b9f2bac8885896901c3
HugoNishimoto07 <89364149+HugoNishimoto07@users.noreply.github.com>
2022-05-01 13:01:06 -0300

    Create Ex4.c

---
+++ Exercício 4/Ex4.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+float CalcTaylor()
+{
+  int n;
+
+
+  float soma = 0;
+  float div;
+  int count = 1;
+
+  for (int d = 1; d < 10; d++)
+    {
+      n = 1;
+      
+      div = pow(n,d)/d;
+      
+      soma = soma + div;
+
+      printf("Iteração %d\n", count);
+      printf("Valor de N: %d\n", n);
+      printf("Valor de D: %d\n", d);
+      printf("Div: %.2f\n", div);
+      printf("Soma: %.2f\n", soma);
+      printf("\n");
+      
+      count++;
+
+    }
+
+  
+  return soma;
+}
+
+int main(void) 
+
+
+{
+
+  float func = CalcTaylor();
+  
+  printf("Série de Taylor(valor sem aproximação de casas decimais): \n %f", func);
+  
+  
+  
+  return 0;
+}
